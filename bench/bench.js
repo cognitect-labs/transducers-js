da821910610ad132859fa0713ee0c33d8b3eaca0
David Nolen <davidnolen@Davids-MacBook-Pro.local>
2014-09-22 22:14:13 -0400

    benchmarks wip

--- bench/bench.js
+++ bench/bench.js
@@ -14,15 +14,83 @@
 
 "use strict";
 
-var transducers = require("../target/transducers.js");
+if(typeof require != "undefined") {
+    var _ = require("../target/transducers.js");
+} else {
+    var _ = transducers;
+}
+
+function log(varArgs) {
+    if(typeof console != "undefined") {
+        console.log.apply(console, Array.prototype.slice.call(arguments, 0));
+    } else {
+        print(Array.prototype.slice.call(argument, 0).join(" "));
+    }
+}
 
 function time(f, iters) {
     iters = iters || 1;
     for(var i = 0; i < iters; i++) {
         var s = new Date();
-        f();
-        console.log("Elapsed "+((new Date()).valueOf()-s.valueOf())+"ms");
-        console.log("----------");
+        var ret = f();
+        log(ret, "elapsed "+((new Date()).valueOf()-s.valueOf())+"ms");
+        log("----------");
     }
 }
 
+function inc(n) { return n + 1; };
+function isEven(n) { return n % 2 == 0; };
+function apush(arr, x) { arr.push(x); return arr; };
+function doubleN(n) { return n + n; };
+function squareN(n) { return n * n; };
+
+log(_.comp(doubleN,squareN)(3));
+log(_.reduce(_.map(inc), apush, [], [0,1,2,3,4,5,6,7,8,9]));
+log(_.reduce(_.filter(isEven), apush, [], [0,1,2,3,4,5,6,7,8,9]));
+log(_.reduce(_.comp(_.map(inc), _.filter(isEven)), apush, [], [0,1,2,3,4,5,6,7,8,9]));
+
+var largeArray = [];
+for(var i = 0; i < 1000000; i++) {
+    largeArray.push(i);
+}
+
+log("for loop, 1 op")
+time(function() {
+    var ret = [];
+    for(var i = 0; i < largeArray.length; i++) {
+        ret.push(inc(largeArray[i]));
+    }
+    return ret.length;
+});
+
+log("native map array, 1 op")
+time(function() {
+    return largeArray.map(inc).length;
+});
+
+log("transduce map large array, 1 op")
+time(function() {
+    return _.reduce(_.map(inc), apush, [], largeArray).length;
+});
+
+log("for loop, 2 ops")
+time(function() {
+    var ret = [];
+    for(var i = 0; i < largeArray.length; i++) {
+        var n = inc(largeArray[i]);
+        if(isEven(n)) {
+            ret.push(n);
+        }
+    }
+    return ret.length;
+});
+
+log("native map/filter array, 2 ops")
+time(function() {
+    return largeArray.map(inc).filter(isEven).length;
+});
+
+log("transduce map large array, 2 ops")
+time(function() {
+    return _.reduce(_.comp(_.map(inc),_.filter(isEven)), apush, [], largeArray).length;
+});
